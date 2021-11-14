6901880ca5e2f68fce56ddf78701e0767bcc4757
kevin <kevinfdezdelanda@gmail.com>
2021-11-14 21:43:44 +0100

    dxc

--- EJ_UD02_Dialogos/app/src/main/java/com/example/ej_ud02_dialogos/MainActivity.java
+++ EJ_UD02_Dialogos/app/src/main/java/com/example/ej_ud02_dialogos/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.ej_ud02_dialogos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements DialogoLogin.OnDialogoConfirmacionListener{
+
+    private boolean sesionIniciada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FragmentManager fragmentManager= getSupportFragmentManager();
-        DialogoLogin dialogoLogin = new DialogoLogin();
-        dialogoLogin.show(fragmentManager,"tagAlerta");
+        sesionIniciada = false;
+        mostrarLogin();
 
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    public void mostrarLogin(){
+        if(!sesionIniciada){
+            FragmentManager fragmentManager= getSupportFragmentManager();
+            DialogoLogin dialogoLogin = new DialogoLogin();
+            dialogoLogin.show(fragmentManager,"tagAlerta");
+        }
     }
 
     @Override
@@ -24,9 +45,12 @@ public class MainActivity extends AppCompatActivity implements DialogoLogin.OnDi
         if(usu.equals("usuario1") && pass.equals("123456")){
             Toast.makeText(this, "Login correcto",
                     Toast.LENGTH_SHORT).show();
+            sesionIniciada = true;
         }else{
             Toast.makeText(this, "Login incorrecto",
                     Toast.LENGTH_SHORT).show();
+            mostrarLogin();
+            sesionIniciada = false;
         }
 
     }
@@ -34,5 +58,35 @@ public class MainActivity extends AppCompatActivity implements DialogoLogin.OnDi
     public void onNegativeButtonClick() {
         Toast.makeText(this, "Botón Negativo pulsado",
                 Toast.LENGTH_SHORT).show();
+        cerrarAplicacion();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            cerrarAplicacion();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void cerrarAplicacion() {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setTitle("¿Realmente desea cerrar la aplicación?")
+                .setCancelable(false)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mostrarLogin();
+                    }
+                })
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {// un listener que al pulsar, cierre la aplicacion
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid()); //Su funcion es algo similar a lo que se llama cuando se presiona el botón "Forzar Detención" o "Administrar aplicaciones", lo cuál mata la aplicación
+                        //finish(); Si solo quiere mandar la aplicación a segundo plano
+                    }
+                }).show();
     }
 }
